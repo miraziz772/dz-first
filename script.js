2c01910cceb291c68f91505e833d16b7fbe57d77
miraziz772 <76822028+miraziz772@users.noreply.github.com>
2020-12-31 23:11:25 +0500

    123

---
+++ script.js
@@ -0,0 +1,17 @@
+var welcome = +prompt('Добро пожаловать!');
+var name = +prompt('Как вас зовут?');
+console.log(name + ' - ваше имя');
+var years = +prompt('Cколько вам лет?');
+console.log(years + ' - ваш возраст');
+var vopros1 = +prompt('реши пример : 7+2=');
+console.log(vopros1 + ' - ваш ответ на первый пример');
+var vopros2 = +prompt('реши пример : 10-2=');
+console.log(vopros2 + ' - ваш ответ на второй пример');
+var vopros3 = +prompt('реши пример : 5*3=');
+console.log(vopros3 + ' - ваш ответ на третий пример');
+var vopros4 = +prompt('реши пример : 8/2=');
+console.log(vopros4 + ' - ваш ответ на четвертый пример');
+var vopros5 = +prompt('реши пример : 20%3=');
+console.log(vopros5 + ' - ваш ответ на пятый пример');
+alert('зайди в консоль, чтобы узнать ответы');
+     
